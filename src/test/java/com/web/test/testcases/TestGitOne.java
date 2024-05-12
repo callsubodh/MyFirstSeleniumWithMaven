59d07c118df6fb362c08afdf33efcfb8ffc7b5eb
callsubodh <callsubodh@gmail.com>
2024-05-12 05:37:48 -0700

    Committed only TestGitOne.java file only

---
+++ src/test/java/com/web/test/testcases/TestGitOne.java
@@ -0,0 +1,13 @@
+package com.web.test.testcases;
+
+import org.testng.annotations.Test;
+
+public class TestGitOne {
+	
+	@Test
+	public void display() {
+		
+		System.out.println("This is display mtd.");
+	}
+
+}
