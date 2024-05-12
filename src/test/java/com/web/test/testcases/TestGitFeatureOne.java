a880e08d1fcb22ff213d594c8c06530587c0cff0
callsubodh <callsubodh@gmail.com>
2024-05-12 09:04:56 -0700

    Committed TestGitFeatureOne.java

---
+++ src/test/java/com/web/test/testcases/TestGitFeatureOne.java
@@ -0,0 +1,13 @@
+package com.web.test.testcases;
+
+import org.testng.annotations.Test;
+
+public class TestGitFeatureOne {
+	
+	@Test
+	public void display() {
+		
+		System.out.println("This is feature one branch display method..");
+	}
+
+}
